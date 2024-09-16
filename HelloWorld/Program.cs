6a039fdbba820f0cac18ef1826786a447e6b7f44
Zätha Malmberg <zathamalmberg@gmail.com>
2024-09-16 10:23:54 +0200

    Chage back to hello world

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,8 +1,2 @@
 ﻿
-Console.WriteLine("This is line 1.");
-Console.WriteLine("This is line 2.");
-Console.WriteLine("This is line 3.");Console.WriteLine("This is line 4.");
-Console.WriteLine("This is line 5.");
-
-
-Console.WriteLine("This is line 6.");
+Console.WriteLine("Hello World");
