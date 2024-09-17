326ca2a6777b7c3219f4471e94c07c961815c10f
Zätha Malmberg <zathamalmberg@gmail.com>
2024-09-17 10:43:18 +0200

    done with 13

---
+++ P13GoTo/Program.cs
@@ -0,0 +1,41 @@
+﻿Random random = new Random();
+int number = random.Next(0, 101);
+//Console.WriteLine(number);
+
+int guesses = 0;
+
+Console.WriteLine("Im thinking of a number guess a number");
+Begining:
+string answer = Console.ReadLine();
+int intAnswer = Convert.ToInt32(answer);
+
+bool isBigger = intAnswer > number;
+bool isSmaller = intAnswer < number;
+bool isTheSame = intAnswer == number;
+
+if (isBigger)
+{
+    guesses++;
+    Console.WriteLine("Your guess is too big. Guess smaller");
+    goto Begining;
+}
+else if (isSmaller)
+{
+    guesses++;
+    Console.WriteLine("Your guess is too small. Guess bigger");
+    goto Begining;
+}
+
+else if (isTheSame)
+{
+    guesses++;
+    Console.WriteLine("Your guess is corect");
+}
+
+else
+{
+    Console.WriteLine("Something went wrong please try again");
+    goto Begining;
+}
+
+Console.WriteLine($"You made {guesses} guesses!");
