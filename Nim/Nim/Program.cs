ad6bc582a5af4802846e401c803521e6f90b4cc2
Zepzo <zathamalmberg@gmail.com>
2024-09-24 10:22:55 +0200

    I think its unbeteble now

--- Nim/Nim/Program.cs
+++ Nim/Nim/Program.cs
@@ -2,42 +2,47 @@
 int intSeed = 123243;
 Random random = new Random(intSeed);
 Console.WriteLine("Welcome to Nim!");
-int matches = 24;
+int matches = 25;
 int loop = 0;
 Start:
-if (loop <= matches)
+if (loop < matches)
 {
     Console.Write("|");
     loop++;
     goto Start;
 }
-else if (matches <= 0)
+
+else
+{   
+    goto Number;
+}
+
+Number:
+loop = 0;
+Console.WriteLine("");
+Console.WriteLine($"How many matches would you like to draw?(1, 2 or 3)({matches})");
+string draw = Console.ReadLine();
+int intDraw = Convert.ToInt32(draw);
+if (intDraw == 1 || intDraw == 2 || intDraw == 3)
+{
+    matches -= intDraw;
+    goto AiStart;
+        
+        
+}
+else
+{
+    Console.WriteLine("Please enter a number between 1, 2 or 3");
+    goto Number;
+}
+
+AiStart:
+if (matches <= 0)
 {
     Console.WriteLine("Oh no! you lost :(");
     goto End;
 }
-else
-{   
-    Number:
-    loop = 0;
-    Console.WriteLine("");
-    Console.WriteLine($"How many matches would you like to draw?(1, 2 or 3)({matches})");
-    string draw = Console.ReadLine();
-    int intDraw = Convert.ToInt32(draw);
-    if (intDraw == 1 || intDraw == 2 || intDraw == 3)
-    {
-        matches -= intDraw;
-        goto AiStart;
-        
-    }
-    else
-    {
-        Console.WriteLine("Please enter a number between 1, 2 or 3");
-        goto Number;
-    }
-}
-AiStart:
-int intNumber1 = random.Next(1, 4);
+int intNumber1 = 4 - intDraw;
 Console.WriteLine($"The AI draws {intNumber1} matches");
 matches -= intNumber1;
 
